e843c177c14564bb4e86343764c8cd17f0811eb2
AdamTMalek <malekadas@gmail.com>
2018-01-21 19:38:35 +0000

    Add tests for the cpm module.

---
+++ tests/loader.py
@@ -0,0 +1,15 @@
+from importlib.machinery import SourceFileLoader
+from importlib.util import spec_from_loader, module_from_spec
+
+
+def load(module):
+    if not isinstance(module, str):
+        raise TypeError("Argument must be of type str, not: " + type(module))
+
+    if len(module) == 0:
+        raise ValueError("Module name cannot be empty")
+
+    spec = spec_from_loader(module, SourceFileLoader(module, "../project/" + module))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
---
+++ tests/test_cpm.py
@@ -0,0 +1,53 @@
+import sys
+import unittest
+from contextlib import contextmanager
+from io import StringIO
+import tests.loader as loader
+
+cpm = loader.load("dupcia")
+
+
+class TestMain(unittest.TestCase):
+    """
+    Test the cpm module
+    """
+
+    @contextmanager
+    def captured_output(self):
+        """
+        Captures standard output and standard error
+        """
+        new_out, new_err = StringIO(), StringIO()
+        old_out, old_err = sys.stdout, sys.stderr
+
+        try:
+            sys.stdout, sys.stderr = new_out, new_err
+            yield sys.stdout, sys.stderr
+        finally:
+            sys.stdout, sys.stderr = old_out, old_err
+
+    def test_no_project_name(self):
+        """
+        Test if the main function accepts no project name to be given.
+        We expect it to return False because the name is required
+        """
+        args = {"cpm"}  # The first argument is the filename we run
+        self.assertFalse(cpm.main(args))
+
+    def test_invalid_name(self):
+        """
+        We expect the method to only accept alphabetic characters.
+        Test with some numbers, special characters to check if the function accepts the input
+        """
+        invalid_names = {"hello world", "53project", "!project", "\"project\""}
+        for name in invalid_names:
+            with self.subTest(name=name):
+                with self.captured_output():  # Use captured_output to silence any error messages
+                    self.assertFalse(cpm.valid_name(name))
+
+    def test_valid_name(self):
+        """
+        Test if name validator accepts valid name
+        """
+        name = "HelloWorld"
+        self.assertTrue(cpm.valid_name(name))
