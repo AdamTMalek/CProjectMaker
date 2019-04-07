c1b824de0f6e37abf9aae85b9296c73384bf4979
AdamTMalek <malekadas@gmail.com>
2019-04-07 17:06:36 +0100

    Refactoring - Make project a class

--- scripts/project.py
+++ scripts/project.py
@@ -1,85 +1,98 @@
 import errno
-import os
 import fileinput
+import os
 from shutil import copyfile
+
+from submanager import Submanager
 from verbose import *
 
-directory = ""
-name = ""
-verbose = None
 
+class Project(Submanager):
+    def __init__(self, verbose_obj, project_name):
+        super().__init__(verbose_obj)
+        self.project_name = project_name
+        self.project_directory = os.path.join(os.getcwd(), self.project_name)
 
-def set_verbose(verbose_obj):
-    if not isinstance(verbose_obj, Verbose):
-        raise TypeError("verbose_obj must be of type {expected}, not {actual}"
-                        .format(expected=Verbose, actual=type(verbose_obj)))
-    global verbose
-    verbose = verbose_obj
+    @staticmethod
+    def add_subparser(subparsers):
+        parser = subparsers.add_parser('project', help='creates/renames project')
+        parser.add_argument('-r', '--rename', help='rename project with [old_name] to [name]', metavar='old_name')
+        parser.add_argument('-v', '--verbose', action='count')
+        parser.add_argument("name", help="target project name")
+        # Set a function that will be called to handle the arguments
+        parser.set_defaults(function=Project.handle_args)
 
-
-def create_folders(project_name):
-    """
-    Creates folders for the project
-    :param project_name: Project name will be used as the name of the main directory
-    :return: True if creating folders finished successfully, False otherwise
-    """
-
-    global directory
-    directory = os.path.join(os.getcwd(), project_name)
-
-    try:
-        os.makedirs(directory)
-    except OSError as error:
-        if error.errno == errno.EEXIST:
-            verbose.print(MessageType.ERROR, "project with the same name already exists", min_level=0)
-            return False
-        if error.errno == errno.EACCES:
-            verbose.print(MessageType.ERROR, "permission denied", min_level=0)
-            return False
+    @staticmethod
+    def handle_args(args, verbose):
+        if args.rename:
+            Project.rename(args.rename, args.name)
         else:
-            raise error
+            project = Project(verbose, args.name)
+            project.create_project()
 
-    os.makedirs(os.path.join(directory, "src"))
-    os.makedirs(os.path.join(directory, "build"))
+    def create_folders(self):
+        """
+        Creates folders for the project
+        :return: True if creating folders finished successfully, False otherwise
+        """
+        try:
+            os.makedirs(self.project_directory)
+        except OSError as error:
+            if error.errno == errno.EEXIST:
+                Verbose.print_any_level(MessageType.ERROR, "project with the same name already exists")
+                return False
+            if error.errno == errno.EACCES:
+                Verbose.print_any_level(MessageType.ERROR, "permission denied")
+                return False
+            else:
+                raise error
 
-    return True
+        os.makedirs(os.path.join(self.project_directory, "src"))
+        os.makedirs(os.path.join(self.project_directory, "build"))
 
+        return True
 
-def create_main_file():
-    if len(directory) == 0:
-        verbose.print(MessageType.ERROR, "project directory doesn't exist", min_level=0)
-        return
+    def create_main_file(self):
+        """
+        Create the main.c file
+        """
+        template = os.path.join(os.path.dirname(__file__), "../templates/main.c.txt")
+        main_file = os.path.join(self.project_directory, "src/main.c")
+        copyfile(template, main_file)
 
-    template = os.path.join(os.path.dirname(__file__), "../templates/main.c.txt")
-    main_file = os.path.join(directory, "src/main.c")
-    copyfile(template, main_file)
+    def create_makefile(self):
+        """
+        Create the makefile
+        """
+        template_path = os.path.join(os.path.dirname(__file__), "../templates/makefile.txt")
+        makefile_path = os.path.join(self.project_directory, "makefile")
 
+        with open(template_path, mode='r') as template:
+            with open(makefile_path, mode='w') as makefile:
+                for line in template.readlines():
+                    line = line.replace("[PROJECT_NAME]", self.project_name)
+                    makefile.write(line)
 
-def create_makefile():
-    template_path = os.path.join(os.path.dirname(__file__), "../templates/makefile.txt")
-    makefile_path = os.path.join(directory, "makefile")
+    def create_project(self):
+        """
+        Create project folders (main folder with src and build in it), main.c and the makefile
+        """
+        if self.create_folders():
+            self.create_main_file()
+            self.create_makefile()
 
-    with open(template_path, mode='r') as template:
-        with open(makefile_path, mode='w') as makefile:
-            for line in template.readlines():
-                line = line.replace("[PROJECT_NAME]", name)
-                makefile.write(line)
+    @staticmethod
+    def rename(old, new):
+        """
+        Rename the project
+        :param old: old (existing) name
+        :param new: new name
+        """
+        if not os.path.isdir(os.path.join(os.getcwd(), old)):
+            Verbose.print_any_level(MessageType.ERROR, "project {} does not exist".format(old), stream=sys.stderr)
+            return
 
-
-def create_project(project_name):
-    global name
-    name = project_name
-    if create_folders(project_name):
-        create_main_file()
-        create_makefile()
-
-
-def rename(old, new):
-    if not os.path.isdir(os.path.join(os.getcwd(), old)):
-        verbose.print(MessageType.ERROR, "project {} does not exist".format(old), min_level=0)
-        return
-
-    os.rename(old, new)
-    makefile_path = (os.path.join(os.getcwd(), new, "makefile"))
-    for line in fileinput.input(makefile_path, inplace=True):
-        print(line.replace(old, new), end='')  # Change the end to an empty string, otherwise it will put another \n.
+        os.rename(old, new)
+        makefile_path = (os.path.join(os.getcwd(), new, "makefile"))
+        for line in fileinput.input(makefile_path, inplace=True):
+            print(line.replace(old, new), end='')  # Change the end to an empty string, otherwise it will put another \n
---
+++ scripts/submanager.py
@@ -0,0 +1,27 @@
+import os
+from abc import ABC, abstractmethod
+
+from verbose import Verbose
+
+
+class Submanager(ABC):
+    @abstractmethod
+    def __init__(self, verbose_obj):
+        """
+        Constructor of any submanager (project/module etc.)
+        :param verbose_obj: Verbose object
+        """
+        if not isinstance(verbose_obj, Verbose):
+            raise TypeError("verbose object should be of type {}, not {}".format(type(Verbose), type(verbose_obj)))
+        self.verbose = verbose_obj
+        self.working_dir = os.getcwd()
+
+    @staticmethod
+    @abstractmethod
+    def add_subparser(subparsers):
+        raise NotImplementedError("add_subparsers is not implemented")
+
+    @staticmethod
+    @abstractmethod
+    def handle_args(args, verbose):
+        raise NotImplementedError("handle_args is not implemented")
